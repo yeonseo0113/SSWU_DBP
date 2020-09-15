99822e7f11b178c26c0d4b907c39985c0673f2a6
yeonseo0113 <70576313+yeonseo0113@users.noreply.github.com>
2020-09-15 22:39:19 +0900

    Create index.php

---
+++ W03-P/index.php
@@ -0,0 +1,49 @@
+<?php
+  $link = mysqli_connect('localhost','root','rootroot','dbp');
+  $query = "SELECT * FROM w02_assignment";
+  $result = mysqli_query($link, $query);
+  $list ='';
+  while ($row = mysqli_fetch_array($result)) {
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+  'title' => 'Welcome',
+  'description' => 'COLOR is ...'
+  );
+   $update_link = '';
+   $delete_link = '';
+
+  if( isset($_GET['id'])) {
+    $query = "SELECT * FROM w02_assignment WHERE id={$_GET['id']}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+    $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
+    $delete_link = '
+      <form action="process_delete.php" method="POST">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type="submit" value="delete">
+      </form>';
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DATABASE</title>
+  </head>
+  <body>
+    <h1><a href="index.php">MY COLOR</a></h1>
+    <ol><?= $list ?></ol>
+    <a href="create.php">create</a>
+    <?= $update_link ?>
+    <?= $delete_link ?>
+    <h2><?= $article['title'] ?></h2>
+    <?= $article['description'] ?>
+  </body>
+</html>
